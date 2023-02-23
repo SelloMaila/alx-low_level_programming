6fa16351776292b826a9e6a884cc024133e5860e
Sello <mailajnr@gmail.com>
2023-02-23 01:02:19 -0800

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets in reverse using putchar
+ * Return: Always 0 (Seccess)
+ */
+
+int main (void)
+{
+	char alpha='z';
+
+	while (alpha >= 'a');
+	{
+		putchar(alpha);
+		alpha++
+	}
+	putchar('\n')
+
+	return (0)
+}	

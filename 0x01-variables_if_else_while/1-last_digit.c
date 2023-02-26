2dc37ed02b060f69a7d835e1877daacaa2745c67
Sello <mailajnr@gmail.com>
2023-02-26 03:11:28 -0800

    real learning

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,21 +13,20 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+ 
+        printf("Last digit of %i is %i and is", n (n % 10))
 
-	printf("Last digit of %i is %i and is ", n, (n % 10));
+		if((n % 10) > 5)
+		{
+			printf("greater than 5"\n)
+		}
+		else if((n % 10) == 0)
+		{
+			printf("equal to 0"\n)
+		}
+		else((n % 10) < 6 )
+		{
+			printf("is less than 6"\n)
+		}
 
-	if ((n % 10) == 0)
-	{
-		printf("0\n");
-	}
-	else if ((n % 10) > 5)
-	{
-		printf("greater than 5\n");
-	}
-	else
-	{
-		printf("less than 6 and not 0\n");
-	}
-
-	return (0);
 }

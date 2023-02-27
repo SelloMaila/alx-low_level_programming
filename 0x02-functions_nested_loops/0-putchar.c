5cb4848eaa5746f21f70093fb64c506b3f4d4ed0
Sello <mailajnr@gmail.com>
2023-02-26 23:32:32 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,17 +5,25 @@
  * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
+
+int _putchar(char c);
+
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
 
-	return (0);
+        return (0);
+}
+
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
 }

a4f4e0d84f0c80de988b1abd42e818dedcb0e8af
Sello <mailajnr@gmail.com>
2023-02-26 23:40:43 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,29 +1,20 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
-
-int _putchar(char c);
-
 int main(void)
 {
-        _putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-        return (0);
-}
-
-int _putchar(char c)
-{
-        return (fwrite(1, &c, 1));
+	return (0);
 }
